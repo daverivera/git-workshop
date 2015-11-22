15bcf4271515a312e54c1e765cb686eecdf87af6
Dave Rivera <daverivera90@gmail.com>
2015-11-21 22:18:36 -0500

    Added gitignore to ignore the build files

--- build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{"\u00cdndic":0,Busqued:0,Contents:0,Modul:0,Pagin:0},titles:["Welcome to Git Workshop&#8217;s documentation!"],titleterms:{Git:0,Indic:0,Welcom:0,Workshop:0,and:0,documentation:0,tabl:0}})
